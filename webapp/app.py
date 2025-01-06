1d3c7523baf50c0ad5d96937605bd316defeaa5d
Zach Perkins <partofaplan@gmail.com>
2025-01-06 11:41:42 -0600

    Added ingress deployments

--- webapp/app.py
+++ webapp/app.py
@@ -19,6 +19,7 @@ GENERATE_API_URL = f"http://{HOST}:{PORT}/api/generate"
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logger.info(f"Generated API URL: {GENERATE_API_URL}")
 
 # Route for the home page
 @app.route('/')
